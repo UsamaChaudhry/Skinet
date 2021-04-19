fd0e1984b3d37b0694b1cd82abb5ce2dbe0f867a
usamabinahmed2898@outlook.com <usamabinahmed2898@outlook.com>
2021-04-19 16:42:23 +0500

    Importing All Files

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Getproducts()
+        {
+            var product = await _context.Product.ToListAsync();
+            return Ok(product);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> Getproduct(int id)
+        {
+            return await _context.Product.FindAsync(id);
+        }
+    }
+}
